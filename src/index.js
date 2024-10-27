74d951dfef4f4971eb0a00d8b6011b0bcff58998
Kurokawa Ryotaro <31917@toyota.kosen-ac.jp>
2024-10-27 23:50:04 +0900

    作ったものを追加

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//手順１:ReactとReactDOMのライブラリーをインポート(import)する。
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+//手順2:index.htmlファイルの<div id="root">要素への参照を取得する。
+//index.htmlファイルの<div id="root">要素を取得します。
+const el = document.getElementById("root");
+
+//手順3:Reactにindex.htmlファイルの<div id="root">要素の参照を渡す。
+const root = ReactDOM.createRoot(el);
+
+//手順4:コンポーネントを作成する。
+//Reactのコンポーネントを一つ作成します。
+//Reactのコンポーネントは、何らかのJSXを返す関数である必要があります。
+
+//手順5:index.htmlファイルの<div id="root">要素の中にコンポーネントを表示させる。
+//定義したAppコンポーネントを画面に表示する。
+root.render(<App />);		
